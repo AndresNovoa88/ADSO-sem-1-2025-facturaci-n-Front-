a039f64eb3c55f52cf45897e7ce53bad54c217f3
AndresNovoa <andresnovoa334@gmail.com>
2025-05-04 19:51:19 -0500

    correccion de visualizacion de la pagina de inicio en blanco

--- frontend/src/layouts/AuthLayout.js
+++ frontend/src/layouts/AuthLayout.js
@@ -1,12 +1,11 @@
-//src/layouts/AuthLayout.js
 import React from 'react';
 import { Layout } from 'antd';
 import { useAuth } from '../contexts/AuthContext';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Outlet } from 'react-router-dom';
 
 const { Content } = Layout;
 
-const AuthLayout = ({ children }) => {
+const AuthLayout = () => {
   const { isAuthenticated } = useAuth();
   const navigate = useNavigate();
 
@@ -15,19 +14,24 @@ const AuthLayout = ({ children }) => {
   }, [isAuthenticated, navigate]);
 
   return (
-    <Layout style={{ 
-      minHeight: '100vh', 
-      background: '#f0f2f5',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center'
-    }}>
-      <Content style={{ 
-        width: '100%',
-        maxWidth: 400,
-        padding: 20
-      }}>
-        {children}
+    <Layout
+      style={{
+        minHeight: '100vh',
+        background: '#f0f2f5',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}
+    >
+      <Content
+        style={{
+          width: '100%',
+          maxWidth: 400,
+          padding: 20
+        }}
+      >
+        {/* Aquí es donde se inyecta el <Login /> */}
+        <Outlet />
       </Content>
     </Layout>
   );
